73f10bfa311259081863f0132db842eec614eefb
Jonny Irwin <git@jbi.im>
2021-10-22 13:02:48 +0100

    initialise project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "brandone": {
+          "50": "#fff2ed",
+          "100": "#ffe8e3",
+          "200": "#d8aca7",
+          "300": "#cea29d",
+          "400": "#c49893",
+          "500": "#ba8e89",
+          "600": "#b0847f",
+          "700": "#a67a75",
+          "800": "#9c706b",
+          "900": "#926661"
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
